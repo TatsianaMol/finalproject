91cc0a7fe600efa413d4ab8cc5bf550d804a55d3
Tatsiana Moloshenok <moloshonock.tanya@mail.ru>
2023-12-02 15:19:49 +0300

    1

---
+++ Documents/шаг/финальный проект/js/file.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu_main');
+menu.onclick = function(event) {
+let elem_menu = document.querySelector('.wow')
+elem_menu.classList.toggle('dropdown-content');
+}
+
+
+
+
